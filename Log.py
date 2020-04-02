7e30bf43fad31f6c037b9b702eab635082b0ed99
Ayesha Ahmad <aahmad9@luc.edu>
2020-04-02 09:40:08 -0500

    added method to look at phenotype obj

--- Log.py
+++ Log.py
@@ -1,3 +1,5 @@
+from phenotype import Phenotype 
+
 class Log():
     '''
     Methods for writing to log file
@@ -7,7 +9,13 @@ class Log():
         self.logfile = open(logfile, 'w')
         
     def write_string(self, string):
-        self.logfile.write('You typed this string: {}\n'.format(string))
+        self.logfile.write('{}\n'.format(string))
         
     def close_log(self):
         self.logfile.close()
+
+    def get_phenotype_parameters(phenotype):
+        self.write_string('Phenotype: ' + phenotype.phenotype)
+        self.write_string('Genome Directory: ' + phenotype.genome_dir)
+        self.write_string('Output Directory: ' + phenotype.output_dir)
+        self.write_string('Genomes: ' + phenotype.genomes)
